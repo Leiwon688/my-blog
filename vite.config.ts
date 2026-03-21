e37a9581e26449d6e0a5fc643ad4ddd3c83baf86
Leiwon688 <wanglei996666@gmail.com>
2026-03-21 15:57:09 +0800

    Restore base URL to /my-blog/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/my-blog/',
   plugins: [react()],
   resolve: {
     alias: {
